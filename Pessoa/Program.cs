bb578ab885e672bb95c4d6c8724dc525d973430d
Shadowpapa <jm_matheus_20@hotmail.com>
2020-06-25 20:17:26 -0300

    Add project files.

---
+++ Pessoa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pessoa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
